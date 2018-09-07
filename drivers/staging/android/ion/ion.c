89db4c71b068604465c61abe0648f9db05a55129
Nikish21 <dasnikish2109@gmail.com>
2018-09-07 20:38:31 +0530

    Fix conflict

--- drivers/staging/android/ion/ion.c
+++ drivers/staging/android/ion/ion.c
@@ -404,12 +404,8 @@ static void ion_handle_get(struct ion_handle *handle)
 }
 
 /* Must hold the client lock */
-<<<<<<< HEAD
-static struct ion_handle* ion_handle_get_check_overflow(struct ion_handle *handle)
-=======
 static struct ion_handle *ion_handle_get_check_overflow(
 					struct ion_handle *handle)
->>>>>>> v4.4.153
 {
 	if (atomic_read(&handle->ref.refcount) + 1 == 0)
 		return ERR_PTR(-EOVERFLOW);
